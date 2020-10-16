c6dd688c2f935aaec05d9d0f8368ce631a1eb789
idam fadilah <idamfadilah.9c@gmail.com>
2020-10-17 04:48:34 +0700

    update

--- storage/urls.py
+++ storage/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.scanner, name="index"),
+    path('', views.scanner, name="storage"),
+    path('put/', views.put, name="put"),
+    # path('move/', views.move, name="move"),
+    # path('out/', views.out, name="out")
 ]
--- storage/views.py
+++ storage/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from WMS.models import *
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 
 from pprint import pprint
 
@@ -12,9 +12,24 @@ from django.db import connection
 from django.db import models
 
 from datetime import datetime
+from json import dumps
+
 
 # Create your views here.
 
 
 def scanner(request):
-    return render(request, 'storage/index.html')
+    data = {
+        "item": list(Item.objects.all().select_related('subcategoryid').values_list('id', 'name', 'subcategoryid__name'))
+    }
+    datas = dumps(data)
+    # if request.is_ajax():
+    #     text = request.GET.get('data')
+    #     return JsonResponse({'Da})
+    return render(request, 'storage/index.html', {"datas": datas})
+
+
+def put(request):
+    data = request.POST.get('codedata', None)
+    print(data)
+    return JsonResponse({'code': data}, status=200)
