1e0ddff2cd048652a85b98e0d5b9623c8c8a47c0
idam fadilah <idamfadilah.9c@gmail.com>
2020-11-23 10:22:31 +0700

    fix static file

---
+++ staticfiles/js/minified/ajax.2b1d4875b33c.js
@@ -0,0 +1,7 @@
+// $(document).ready(function () {
+//     console.log("lol" + document.getElementById('dodo').value);
+//     $("#code").click(function () {
+//         // var data = JSON.parse($("#data").val())
+//         console.log($("#data").val());
+//     });
+// });
