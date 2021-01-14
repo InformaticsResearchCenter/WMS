aac649ae7e593a543868e5617e9ae6daaa600cf2
rizkydoang <mriski889@gmail.com>
2021-01-14 11:34:13 +0700

    Email verifikasi

--- WMS/settings.py
+++ WMS/settings.py
@@ -13,6 +13,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 import os
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# Membaca file .env
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -154,6 +162,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+#------------ Email --------------
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER='wmspoltekpos@gmail.com'
+DEFAULT_FROM_EMAIL='wmspoltekpos@gmail.com'
+EMAIL_HOST_PASSWORD='wmspoltekpos17'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
